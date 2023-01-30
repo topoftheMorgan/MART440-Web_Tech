1bb1c2610745486e7e09d5f59e09677c6f0f0098
Morgan Bowman <77422567+topoftheMorgan@users.noreply.github.com>
2023-01-29 20:50:27 -0700

    Add files via upload

---
+++ week-2/script.js
@@ -0,0 +1,16 @@
+// Author: Morgan Bowman
+// Date: 1/29/23
+
+// console.log list
+console.log("JavaScript Rules!");
+
+const websights = ["twitch.tv", "youtube.com", "etsy.com"]
+console.log("websights");
+
+const games = ["Heartbound", "Rummy Kub", "Any card game"]
+console.log("games");
+
+const art = ["Dasha Bough", "Leonardo da Vinci", "Walt Disney"]
+console.log("art");
+
+
