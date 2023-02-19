00d93a4f3cd318a6a9989fbc957d52ec45ef1a29
Morgan Bowman <77422567+topoftheMorgan@users.noreply.github.com>
2023-02-19 13:02:46 -0700

    Add files via upload

---
+++ week-5/main.js
@@ -0,0 +1,43 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+
+var blankImagePath = "./images/card.png";
+
+var actualImages = new Array("image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10");
+    
+function printBlanks()
+{
+    //make random image 
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+}
+
+function createRandomImageArray()
+{
+    // array of actual images
+    var actualImagePath = ["images/bird.webp", "images/bunny.jpg", "images/cat.jpeg", "images/cheeta.webp", "images/duck.jpeg", "images/fox.jpeg", "images/frog.jpeg", "images/hippo.jpeg", "images/owl.jpeg", "images/panda.jpeg"];
+
+    var count = [0,0];
+
+    while(actualImages.length < 10)
+    {
+
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+
+        if(count[randomNumber] < 8)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+        
+}
+
