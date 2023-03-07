6624b0e418e4e7084d1575858393e369b2a3f8d2
Morgan Bowman <77422567+topoftheMorgan@users.noreply.github.com>
2023-03-06 20:39:59 -0700

    Add files via upload

---
+++ week-6/main.js
@@ -0,0 +1,120 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
+var blankImagePath = "./images/card.png";
+var actualImages = new Array("images/bird.webp", "images/bunny.jpg", "images/cat.jpeg", "images/cheeta.webp", "images/duck.jpeg", "images/cat.jpeg", "images/bird.webp", "images/cheeta.webp", "images/bunny.jpg", "images/duck.jpeg");
+var firstNumber = -1;
+var secondNumber = -1;
+var score = 0;
+var allFound = 0;
+// JSON
+var player = {"firstname":"", "lastname":"", "age":0, "score":0};
+
+    
+function printBlanks()
+{
+    //make random image 
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+}
+
+function createRandomImageArray()
+{
+    // array of actual images
+    var actualImagePath = ["images/bird.webp", "images/bunny.jpg", "images/cat.jpeg", "images/cheeta.webp", "images/duck.jpeg", "images/fox.jpeg", "images/frog.jpeg", "images/hippo.jpeg", "images/owl.jpeg", "images/panda.jpeg"];
+
+    var count = [0,0];
+
+    while(actualImages.length < 10)
+    {
+
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+
+        if(count[randomNumber] < 8)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+}
+
+function flipImage(number)
+{
+    if(firstNumber >= 0)
+    {
+        secondNumber = number;
+        document.getElementById(imageTags[number]).src = actualImages[secondNumber];
+        
+    }
+    else if(firstNumber < 0) // make the first image appear
+    {
+        firstNumber = number;
+        document.getElementById(imageTags[firstNumber]).src= actualImages[firstNumber];
+    
+    }
+
+    // check to see if the images do not match
+    if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        score++;
+        setTimeout(imagesDisappear, 1000); // calls a method after 1 second
+    }
+    // check to see if the images do match
+    else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
+    {
+        score++;
+        allFound++;
+        
+        firstNumber = -1;
+        secondNumber = -1;
+        if(allFound == actualImages.length/2)
+        {  
+            player.score = score;
+            localStorage.setItem("playerInfo", JSON.stringify(player));
+            window.location = "index.html";
+        }
+    }
+        
+}
+
+function imagesDisappear()
+{
+
+    console.log(firstNumber);
+    document.getElementById(imageTags[firstNumber]).src = blankImagePath;
+    document.getElementById(imageTags[secondNumber]).src = blankImagePath;
+    firstNumber = -1;
+    secondNumber = -1;
+}
+
+// add to the JSON from the textboxes
+function addToPlayer()
+{
+    var firstName = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var age = document.getElementById("txtAge").value;
+   
+    player.firstname = firstName;
+    player.lastname = lastName;
+    player.age = age;
+    localStorage.setItem("playerInfo", JSON.stringify(player));
+    window.location = "index.html";
+}
+
+// get the information out of JSON
+function playerInfo()
+{
+    var playerInformation = localStorage.getItem("playerInfo");
+    player = JSON.parse(playerInformation);
+    var str = "Name: " + player.firstname + " " + player.lastname + "<br>" +
+    "Age: " + player.age + "<br>" +
+    "Score: " + player.score;
+    if(document.getElementById("endInformation") != null)
+    {
+        document.getElementById("endInformation").innerHTML = str;
+    }
+    
+   
+}
