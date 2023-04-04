a1a22a36d3dd328ec9ee4b1d8da174f1701918e2
Morgan Bowman <77422567+topoftheMorgan@users.noreply.github.com>
2023-04-03 23:22:28 -0600

    Add files via upload

--- week-9/jsonajax.js
+++ week-9/jsonajax.js
@@ -3,13 +3,19 @@ $(document).ready(function () {
         $("#pillowInformation").load("data/pillow.json", function(responseText){
             var pillow = JSON.parse(responseText);
                 $("#pillowInformation").html("Creator: " + pillow.Creator 
-            + "<br>Product:" + pillow.Product + pillow.Size + "<br>width:" + pillow.Size.width + "<br>height:" 
+            + "<br>Product:" + pillow.Product + "<br>width:" + pillow.Size.width + "<br>height:" 
             + pillow.Size.height + "<br>Types:<br>" +
             pillow.Types[0] + "<br>" + pillow.Types[1] + "<br>" + pillow.Types[2] + "<br>" + pillow.Types[3]);
-        });
+        }).blue(); // call the plugin here
     });
 });
 
+/* jquery plugin */
+
+$.fn.blue = function() {
+    this.css( "color", "blue" );
+};
+ 
 
 
 /* function fadeText()
--- week-9/main.js
+++ week-9/main.js
@@ -28,8 +28,14 @@ $("button").click(function(){
 
 function showPillowInfo()
 {
-    $("#pillowInformation").html("Creator: " + pillow.Creator 
+   /* $("#pillowInformation").html("Creator: " + pillow.Creator 
     + "<br>Product:" + pillow.Product + pillow.Size + "<br>width:" + pillow.Size.width + "<br>height:" 
+    + pillow.Size.height + "<br>Types:<br>" +
+    pillow.Types[0] + "<br>" + pillow.Types[1] + "<br>" + pillow.Types[2] + "<br>" + pillow.Types[3]);
+    */
+
+    $("#pillowInformation").html("Creator: " + pillow.Creator 
+    + "<br>Product:" + pillow.Product + "<br>width:" + pillow.Size.width + "<br>height:" 
     + pillow.Size.height + "<br>Types:<br>" +
     pillow.Types[0] + "<br>" + pillow.Types[1] + "<br>" + pillow.Types[2] + "<br>" + pillow.Types[3]);
 }
